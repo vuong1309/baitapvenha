d7bcdf15aae44604740dc51b61142273533e25f5
Vuong Nguyen <nvbkhnkthh6@gmail.com>
2023-06-12 23:51:31 +0700

    update btvn buoi 12

--- lesson12/index.js
+++ lesson12/index.js
@@ -1,5 +1,5 @@
 //trỏ vào thẻ input
-const inputEle = document.getElementById("taskInput");
+const inputEles = document.getElementById("taskInput");
 
 //trỏ đến nút bấm "add task"
 const addEles = document.getElementById("addTask");
@@ -9,11 +9,15 @@ const showText = document.getElementById("taskList");
 
 //tạo sự kiện khi click vào "add task" thì sẽ sinh ra 1 li mới
 addEles.addEventListener("click", () => {
-    if (inputEle.innerText !== "") {
+    if (inputEles.value !== "") {
         //tạo 1 thẻ li mới
         const inputText = document.createElement("li");
-        inputText.innerText = inputEle.value;
+        inputText.innerText = inputEles.value;
         showText.appendChild(inputText);
+        let arr = [];
+        arr = JSON.parse(localStorage.getItem('info')) || [];
+        arr.push(inputEles.value.trim());
+        localStorage.setItem('info', JSON.stringify(arr));
     }
 })
 
